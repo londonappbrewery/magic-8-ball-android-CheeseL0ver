3513d451e415335fec1865fd29f0cfa61d304508
Shandon Anderson <andersonshandon@flygone.io>
2017-11-26 19:30:32 -0500

    Completion of 8-Ball App Challenge

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -6,16 +6,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button askButton;
-
-    ImageView eightBall;
-
-    int[] ballArray = {
+    final int[] ballArray = {
             R.drawable.hdpi_ball1,
             R.drawable.hdpi_ball2,
             R.drawable.hdpi_ball3,
@@ -23,14 +21,18 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.hdpi_ball5
     };
 
+    ArrayList<Integer> wordArray = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        eightBall = (ImageView) findViewById(R.id.image_eightBall);
+        final ImageView eightBall = (ImageView) findViewById(R.id.image_eightBall);
 
-        askButton = askButton = (Button) findViewById(R.id.askButton);
+        final Button askButton = (Button) findViewById(R.id.askButton);
+
+        final TextView message = findViewById(R.id.bannerMessage);
 
         askButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Eightball",String.format("The number is: %d",number));
 
                 eightBall.setImageResource(ballArray[number]);
+
+                //Toggle the visibility of an object
+//                if (message.getVisibility() == View.INVISIBLE)
+//                {
+//                    message.setVisibility(View.VISIBLE);
+//                }
+//                else
+//                {
+//                    message.setVisibility(View.INVISIBLE);
+//                }
             }
         });
 
